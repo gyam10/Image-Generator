3ddf1084275536fafa2160137250705e8dcadc3d
Gyamjo Dong <nischallama222@gmail.com>
2023-02-13 16:22:15 +0545

    first commit

---
+++ frontend/src/components/index.js
@@ -0,0 +1,5 @@
+import Card from "./card";
+import FormField from "./formfield";
+import Loader from "./loader";
+
+export { Card, FormField, Loader };
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./home";
+import CreatePost from "./createpost";
+
+export { Home, CreatePost };
